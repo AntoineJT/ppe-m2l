9bb8a21e64e08aeea51ab2f37d7c1828f547c246
mathisprin <47419841+mathisprin@users.noreply.github.com>
2019-02-07 15:07:33 +0100

    Add files via upload

---
+++ Mission6/formPrestation.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>ajouter une prestation</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	</head>
+<body>
+	<h2>Ajouter une prestation</h2>
+    <p>
+Libellé	&nbsp &nbsp &nbsp &nbsp &nbsp 
+<br />
+<FORM action="..." method="post">
+code
+&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <INPUT TYPE="entier" NAME="codePrestation">
+<br />
+<br />
+libelle
+&nbsp &nbsp &nbsp  &nbsp &nbsp <INPUT TYPE="entier" NAME="libellePrestation">
+<br />
+<br />
+prix unitaire
+<INPUT TYPE="entier" NAME="puPrestation"> 
+<br />
+<br />
+<INPUT TYPE="submit" VALUE="Valider">
+<a href="...">
+</FORM>	
+</body>
+</html>
